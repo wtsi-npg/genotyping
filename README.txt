44fbcb168369dcd804a4bd69b2cb86c3452aaff6
Keith James <kdj@sanger.ac.uk>
2012-04-18 15:58:40 +0100

    Added version number

--- README.txt
+++
@@ -1,6 +0,0 @@
-
-Components of the WTSI genotype analysis pipeline.
-
-This pipeline is designed to process large volumes of raw genotyping
-data into genotype calls. The platform supported is Illumina Infinium
-genotyping microarrays, supplemented with Sequenom genotyping.
--- src/ruby/genotyping-workflows/lib/genotyping/version.rb
+++ src/ruby/genotyping-workflows/lib/genotyping/version.rb
@@ -1,4 +1,4 @@
-#--
+#-- encoding: UTF-8
 #
 # Copyright (c) 2012 Genome Research Ltd. All rights reserved.
 #
@@ -9,27 +9,14 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-require 'percolate'
-
 module Genotyping
-  include Percolate
-  include Tasks
-  include Utilities
+  VERSION = '0.1.0'
 end
 
-require 'genotyping/version'
-require 'genotyping/exceptions'
-
-require 'genotyping/tasks'
-require 'genotyping/sim'
-require 'genotyping/workflows'
-
-require 'genotyping/tasks/genotype_call'
-require 'genotyping/tasks/illuminus'
