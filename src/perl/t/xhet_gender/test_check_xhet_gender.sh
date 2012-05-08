c1ff1b4fad6392f0222b22265715d07826babbf4
Iain Bancarz <ib5@sanger.ac.uk>
2012-05-08 16:48:35 +0100

    Added test of .json format input data

--- src/perl/t/xhet_gender/test_check_xhet_gender.sh
+++ src/perl/t/xhet_gender/test_check_xhet_gender.sh
@@ -12,31 +12,33 @@
 # construct command-line options and run check_xhet_gender script
 SCRIPTDIR="../../bin/"
 SCRIPTNAME="check_xhet_gender.pl"
-INPATH="./sample_xhet_gender.txt"
+INPUTDIR="."
 OUTPUTDIR="./testGenderOutput"
 REFDIR="."
-CMD="perl ${SCRIPTDIR}${SCRIPTNAME} --input=$INPATH --output_dir=${OUTPUTDIR}"
-echo $CMD
-# check for required input
-if [[ ! -e $INPATH ]]
-then
-    echo "Input $INPATH not found!"
-    exit 1
-fi
 
-$CMD
-# did perl script run successfully?
-if [ $? -eq 0 ]; then 
-    echo -e "OK\tcheck_xhet_gender\tExecution test"
-else
-    echo -e "ERROR\tcheck_xhet_gender\tExecution test"
-fi
-# do new output and reference output differ?  (Tests sample gender assignment, not detailed model params.)
-OLD_GENDER=${REFDIR}/sample_xhet_gender_model_ref.txt
-NEW_GENDER=${OUTPUTDIR}/sample_xhet_gender_model.txt
-diff --brief $OLD_GENDER $NEW_GENDER #&> /dev/null
-if [ $? -eq 0 ]; then 
-    echo -e "OK\tcheck_xhet_gender\tOutput test"
-else
-    echo -e "ERROR\tcheck_xhet_gender\tOutput test"
-fi
+for INPATH in $INPUTDIR/sample_xhet_gender.txt $INPUTDIR/sample_xhet_gender.json; do
+    CMD="perl ${SCRIPTDIR}${SCRIPTNAME} --input=$INPATH --output_dir=${OUTPUTDIR}"
+    echo $CMD
+    # check for required input
+    if [[ ! -e $INPATH ]]
+    then
+	echo "Input $INPATH not found!"
+	exit 1
+    fi
+    $CMD
+    # did perl script run successfully?
+    if [ $? -eq 0 ]; then 
+	echo -e "OK\tcheck_xhet_gender:$INPATH\tExecution test"
+    else
+	echo -e "ERROR\tcheck_xhet_gender:$INPATH\tExecution test"
+    fi
+    # do new output and reference output differ?  (Tests sample gender assignment, not detailed model params.)
+    OLD_GENDER=${REFDIR}/sample_xhet_gender_model_ref.txt
+    NEW_GENDER=${OUTPUTDIR}/sample_xhet_gender_model.txt
+    diff --brief $OLD_GENDER $NEW_GENDER #&> /dev/null
+    if [ $? -eq 0 ]; then 
+	echo -e "OK\tcheck_xhet_gender:$INPATH\tOutput test"
+    else
+	echo -e "ERROR\tcheck_xhet_gender:$INPATH\tOutput test"
+    fi
+done
