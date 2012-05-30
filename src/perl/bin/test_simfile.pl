653861852400a9708c51b332a8f5a46edc9ea708
Iain Bancarz <ib5@sanger.ac.uk>
2012-05-30 11:32:51 +0100

    Minor changes to subroutine call

--- src/perl/bin/test_simfile.pl
+++ src/perl/bin/test_simfile.pl
@@ -1,18 +1,21 @@
 #! /usr/bin/perl
 
+# Author:  Iain Bancarz, ib5@sanger.ac.uk
+# May 2012
+
+# simple test script for SimFiles module
+
 use warnings;
 use strict;
-
 use WTSI::Genotyping::QC::SimFiles;
-
 
 my $fh;
 open $fh, "< $ARGV[0]";
 my @fields = WTSI::Genotyping::QC::SimFiles::readHeader($fh);
 foreach my $field (@fields) { print $field."\n"; }
-#my $blockSize = WTSI::Genotyping::QC::SimFiles::findBlockSize(@fields);
 print "#####\n";
 foreach my $i (0..4) {
-    WTSI::Genotyping::QC::SimFiles::readBlock($fh, $i, \@fields);
+    my @items = WTSI::Genotyping::QC::SimFiles::readBlock($fh, $i, \@fields);
+    print join(' # ', @items)."\n";
 }
 close $fh;
