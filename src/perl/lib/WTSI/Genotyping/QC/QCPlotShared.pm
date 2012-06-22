82e498d43852fec20ed902ed3dbb3e403934a33b
Iain Bancarz <ib5@sanger.ac.uk>
2012-06-22 11:03:09 +0100

    Changed QC metric names/files

--- src/perl/lib/WTSI/Genotyping/QC/QCPlotShared.pm
+++ src/perl/lib/WTSI/Genotyping/QC/QCPlotShared.pm
@@ -17,11 +17,28 @@ $xydiff = "xydiff.txt"; # xydiff output file in new qc
 $mainIndex = 'index.html';
 $plateHeatmapDir = 'plate_heatmaps';
 $plateHeatmapIndex = 'index.html'; # written to $plateHeatmapDir, not main output directory
+$duplicates = 'duplicate_summary.txt';
+$idents = 'identity_check_results.txt'; 
+$genders = 'sample_xhet_gender.txt';
 
-# standard qc thresholds
-$minCR = 0.95;
-$maxHetSd = 3;
-$maxXydiffSd = 3;
-$minSnpsForIdent = 10;
-$minIdentity = 0.9;
+# set of allowed QC metric names (long and short versions)
+@qcMetricNames = qw(call_rate heterozygosity duplicate identity gender xydiff);
+%qcMetricNames;
+foreach my $name (@qcMetricNames) { $qcMetricNames{$name}=1; } # convenient for checking name legality
+%qcMetricNamesShort = ($qcMetricNames[0] => 'C',
+		       $qcMetricNames[1] => 'H',
+		       $qcMetricNames[2] => 'D',
+		       $qcMetricNames[3] => 'I',
+		       $qcMetricNames[4] => 'G',
+		       $qcMetricNames[5] => 'X',
+    );
+%qcMetricInputs = ($qcMetricNames[0] => $sampleCrHet,
+		   $qcMetricNames[1] => $sampleCrHet,
+		   $qcMetricNames[2] => [$sampleCrHet, $duplicates],
+		   $qcMetricNames[3] => $idents,
+		   $qcMetricNames[4] => $genders,
+		   $qcMetricNames[5] => $xydiff,
+    );
+
+# standard qc thresholds are in .json file
 # duplicate threshold is currently hard-coded in /software/varinf/bin/genotype_qc/pairwise_concordance_bed
