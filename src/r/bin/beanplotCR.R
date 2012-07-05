9393927d90da1c35c6fea27d2eac54b5e05c3ba5
Iain Bancarz <ib5@sanger.ac.uk>
2012-07-05 10:47:34 +0100

    Correctly handle 100% CR; deactivate inappropriate attempts by beanplot to take logs

--- src/r/bin/beanplotCR.R
+++ src/r/bin/beanplotCR.R
@@ -12,11 +12,14 @@ data <- read.table(args[1])
 title <-  args[2]
 outpath <- args[3]
 plate <- data$V1
-qmax = 40 # max CR quality for plot
-q <- -10*log10(1 - data$V2) # call rate, converted to phred scale
-q[q>qmax] <- qmax # truncate call rate if > Q40 (!)
+qmax <- 40 # max CR quality for plot
+crMax <- 1 - 10**(-qmax/10) # truncate call rate if > Q40 (could have 100% CR on few SNPs)
+cr <- data$V2
+cr[cr>crMax] <- crMax
+q <- -10*log10(1 - cr) # call rate, converted to phred scale
 png(outpath, height=800, width=800, pointsize=18)
 library(beanplot)
-beanplot(q~plate, horizontal=TRUE, las=1, col=c(3,1), ylim=c(0,40), xlab="Call rate (Phred scale)", main=paste("Sample CR by plate: ", title, "\n\n" )) # extra "\n\n" is a hack to correct spacing; TODO fix this properly
+beanplot(q~plate, horizontal=TRUE, las=1, col=c(3,1), ylim=c(0,40), xlab="Call rate (Phred scale)", main=paste("Sample CR by plate: ", title, "\n\n" ), log="")
+# extra "\n\n" is a hack to correct spacing; log="" prevents inappropriate attempts to take logs
 axis(3, at=c(0,10,20,30,40), labels=c('No data', '90%', '99%', '99.9%', '99.99%')) # alternate scale on top
 dev.off()
