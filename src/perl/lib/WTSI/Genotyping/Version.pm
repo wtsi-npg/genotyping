dfd2fb0d766acce2e16e3e3c321563cf28eb7406
Iain Bancarz <ib5@sanger.ac.uk>
2012-12-05 15:31:29 +0000

    Write pipeline version number to QC directory

---
+++ src/perl/lib/WTSI/Genotyping/Version.pm
@@ -0,0 +1,36 @@
+
+package WTSI::Genotyping::Version;
+
+use strict;
+use warnings;
+use Carp;
+
+use vars qw/$VERSION $YEAR @ISA @EXPORT_OK/;
+use Exporter;
+@ISA = qw/Exporter/;
+@EXPORT_OK = qw/version_text write_version_log/;
+
+$VERSION = '0.7.0';
+$YEAR = '2012'; # year of last update
+
+sub version_text {
+    my $text = "WTSI Genotyping Pipeline version $VERSION\n".
+        "Copyright (c) $YEAR Genome Research Ltd. All rights reserved.\n";
+    return $text;
+}
+
+sub write_version_log {
+    # record version in given directory
+    my $logdir = shift;
+    if (!(-e $logdir && -d $logdir)) { 
+        croak "Invalid log directory \"$logdir\"";
+    }
+    my $logpath =  $logdir."/version.log";
+    open my $log, ">", $logpath || 
+        croak "Cannot open version log \"$logpath\"";
+    print $log version_text();
+    close $log || croak "Cannot close version log \"$logpath\"";
+    return 1;
+}
+
+return 1;
