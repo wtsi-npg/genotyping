b902914499cec8064fbddfa22331803ea0467990
Iain Bancarz <ib5@sanger.ac.uk>
2013-03-25 17:09:43 +0000

    Initial commit

---
+++ src/perl/bin/write_snp_metadata.pl
@@ -0,0 +1,162 @@
+#!/software/bin/perl
+
+#
+# Copyright (c) 2013 Genome Research Ltd. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Parse a SNP manifest .csv file and write metadata in .json format
+# Input: The manifest
+# Outputs:
+# 1. SNP manifest, converted to .json format
+# 2. Chromosome boundaries with respect to position in SNP manifest
+
+use strict;
+use warnings;
+use Carp;
+use Getopt::Long;
+use Log::Log4perl qw(:easy);
+use JSON;
+
+Log::Log4perl->easy_init($ERROR);
+
+sub byChromosomePosition {
+    # use to sort manifest into (chromosome, position) order
+    # assumes chromosome identifiers are numeric
+    # $a, $b are global variables used by Perl sort
+    my %snpA = %$a;
+    my %snpB = %$b;
+    my $result = $snpA{'chromosome'} <=> $snpB{'chromosome'} ||
+        $snpA{'position'} <=> $snpB{'position'};
+    return $result;
+}
+
+sub findChromosomeBounds {
+    # find boundaries (start/finish indices) of chromosomes wrt sorted manifest
+    my @manifest = @_;
+    my %chromosomes = ();
+    my @bounds = ();
+    my $start = 0;
+    my $lastChr = 'NULL';
+    for (my $i=0; $i<@manifest; $i++) {
+        my %snp = %{$manifest[$i]};
+        my $chr = $snp{'chromosome'};
+        if ($lastChr ne 'NULL' && $chr!=$lastChr) { # end of previous chromosome
+            push (@bounds, { 'chromosome' => $lastChr,
+                             'start' => $start,
+                             'end' => $i });
+            $start = $i;
+            if ($chromosomes{$chr}) {
+                croak("Inconsistent chromosome name at position $i: $chr\n");
+            } else {
+                $chromosomes{$chr} = 1;
+            }
+        }
+        if ($i+1==@manifest) { # end of final chromosome
+            push (@bounds, { 'chromosome' => $chr,
+                             'start' => $start,
+                             'end' => $i+1 });
+        }
+        $lastChr = $chr;
+    }
+    return @bounds;
+}
+
+sub numericChromosome {
+    # convert chromosome identifiers to numeric values
+    # used by Plink, and needed for manifest sort
+    my $chr = shift;
+    if ($chr eq 'X') { $chr = 23; }
+    elsif ($chr eq 'Y') { $chr = 24; }
+    elsif ($chr eq 'XY') { $chr = 25; }
+    elsif ($chr eq 'MT') { $chr = 26; }
+    return $chr;
+}
+
+sub readManifest {
+    # read manifest bpm.csv
+    # .csv fields: Index,Name,Chromosome,Position,GenTrain Score,SNP,ILMN Strand,Customer Strand,NormID
+    # for each SNP, parse: Name,Chromosome,Position,AlleleA,AlleleB,NormID
+    my $inPath = shift;
+    my $verbose = shift;
+    my @manifest;
+    my %indices = (name => 1,
+                   chromosome => 2,
+                   position => 3,
+                   snp => 5,
+                   norm_id => 8
+        );
+    my $i = 0;
+    open my $in, "<", $inPath || croak "Cannot read input path $inPath";
+    while (<$in>) {
+        $i++;
+        if ($verbose && $i % 10000 == 0) { print "$i lines read.\n"; }
+        if ($i == 1) { next; }
+        $_ =~ s/\s+$//g; # remove whitespace from end of line
+        my %snp;
+        my @fields = split /,/;
+        foreach my $key (qw/name chromosome position norm_id/) {
+            $snp{$key} = $fields[$indices{$key}];
+        }
+        $snp{'chromosome'} = numericChromosome($snp{'chromosome'});
+        my $alleles = $fields[$indices{'snp'}];
+        $alleles =~ s/\W//g; # remove nonword characters
+        my @alleles = split('', $alleles);
+        if (@alleles!=2) { 
+            croak "Failed to find 2 alleles at manifest line ".$i."\n"; 
+        }
+        foreach my $allele (@alleles) {
+            if ($allele !~ /[A-Z]/) { # may include letters other than ACGT
+                croak "Invalid allele character at manifest line ".$i."\n";
+            }
+        }
+        $snp{'allele_a'} = $alleles[0];
+        $snp{'allele_b'} = $alleles[1];
+        push(@manifest, \%snp);
+    }
+    close $in || croak "Cannot close input path $inPath";
+    return @manifest;
+}
+
+sub sortManifest {
+    # must be done before finding chromosome boundaries
+    return sort byChromosomePosition @_;
+}
+
+sub run {
+    my ($manifest, $chrJson, $snpJson, $verbose, $out, $start);
+    $start = time();
+    GetOptions('manifest=s' => \$manifest,
+               'chromosomes=s' => \$chrJson,
+               'snp=s'=> \$snpJson,
+               'verbose' => \$verbose,
+        );
+    my @manifest = sortManifest(readManifest($manifest, $verbose));
+    if ($snpJson) {
+        open $out, ">", $snpJson || croak "Cannot open output $snpJson";
+        print $out to_json(\@manifest);
+        close $out || croak "Cannot close output $snpJson";
+    }
+    if ($chrJson) {
+        my @bounds = findChromosomeBounds(@manifest);
+        open $out, ">", $chrJson || croak "Cannot open output $chrJson";
+        print $out to_json(\@bounds);
+        close $out || croak "Cannot close output $chrJson";
+    }
+    my $duration = time() - $start;
+    if ($verbose) { print "Finished. Duration: $duration s.\n"; }
+}
+
+run();
