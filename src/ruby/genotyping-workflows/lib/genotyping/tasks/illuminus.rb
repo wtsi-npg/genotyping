f910e12a0b993a977bf601949d5f5fcb800655a7
Iain Bancarz <ib5@sanger.ac.uk>
2013-04-16 12:06:04 +0100

    Removed modification to end index for simtools

--- src/ruby/genotyping-workflows/lib/genotyping/tasks/illuminus.rb
+++ src/ruby/genotyping-workflows/lib/genotyping/tasks/illuminus.rb
@@ -92,7 +92,7 @@ module Genotyping::Tasks
            :outfile => '-',
            :man_dir => manifest, # path to file, despite the name!
            :start => range.begin,
-           :end => range.end - 1 } # switch to simtools range convention
+           :end => range.end }
         end
 
         illuminus_wrap_args = partitions(output, snp_ranges.size).collect do |part|
