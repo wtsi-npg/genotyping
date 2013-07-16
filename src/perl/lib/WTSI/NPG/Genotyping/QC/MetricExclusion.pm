0d7a656f15171ef1317190c66f9f8329efe52ea6
Iain Bancarz <ib5@sanger.ac.uk>
2013-07-16 16:38:08 +0100

    Added first draft of method to filter multiple metrics

--- src/perl/lib/WTSI/NPG/Genotyping/QC/MetricExclusion.pm
+++ src/perl/lib/WTSI/NPG/Genotyping/QC/MetricExclusion.pm
@@ -43,6 +43,36 @@ sub filterMetricMinimum {
         }
     }
     # now use %samplePass to update database
+    updateDatabase($dbPath, \%samplePass);
+}
+
+sub filterMetricsMultiple {
+    # filter on multiple metrics
+    my ($dbPath, $configPath, $resultPath, $metricThresholdsRef) = @_;
+    my %samplePass;
+    my %metricThresholds = %{$metricsThresholdsRef};
+    my %metricResults = readMetricResultHash($resultPath, $configPath);
+    foreach my $uri (keys %metricResults) {
+        my %results = %{$metricResults{$uri}};
+        foreach my $metric (keys %metricThresholds) {
+            my ($threshold, $type) = @{$metricThresholds{$metric}};
+            my @result = @{$results{$metric}};
+            if ($type=='min' && $result[1] >= $threshold) {
+                $samplePass{$uri} = 1;
+            } elif (($type=='max' && $result[1] <= $threshold)) {
+                $samplePass{$uri} = 1;
+            } else {
+                $samplePass{$uri} = 0;
+            }
+        }
+    }
+    updateDatabase($dbPath, \%samplePass);
+}
+
+sub updateDatabase {
+    # update pipeline db with pass/fail of each sample
+    my $dbPath = shift;
+    my %samplePass = %{ shift() };
     # samples which were previously excluded should *remain* excluded
     my $db = getDatabaseObject($dbPath); # uses default .ini path
     my @samples = $db->sample->all;
