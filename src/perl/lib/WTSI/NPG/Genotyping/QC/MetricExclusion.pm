b30a3191ec9d063a2ef117b139cb2ec5671adbef
Iain Bancarz <ib5@sanger.ac.uk>
2013-07-18 14:41:33 +0100

    Major rewrite to allow more flexible QC filtering

--- src/perl/lib/WTSI/NPG/Genotyping/QC/MetricExclusion.pm
+++ src/perl/lib/WTSI/NPG/Genotyping/QC/MetricExclusion.pm
@@ -9,64 +9,126 @@ package WTSI::NPG::Genotyping::QC::MetricExclusion;
 use strict;
 use warnings;
 use Carp;
-use WTSI::NPG::Genotyping::QC::QCPlotShared qw/getDatabaseObject 
-    readMetricResultHash/;
+use WTSI::NPG::Genotyping::QC::QCPlotShared qw/decode_json getDatabaseObject 
+    meanSd readMetricResultHash parseThresholds readFileToString/;
 use Log::Log4perl qw(:easy);
 use Exporter;
 
 Log::Log4perl->easy_init($ERROR);
 
 our @ISA = qw/Exporter/;
-our @EXPORT_OK = qw/filterCR/;
+our @EXPORT_OK = qw/runFilter/;
 
-sub filterCR {
-    # filter on call rate
-    my @args = @_;
-    my $metric = 'call_rate';
-    push(@args, $metric);
-    filterMetricMinimum(@args);
-}
-
-sub filterMetricMinimum {
-    # filter with given metric and minimum threshold
-    # arguments: SQLite DB, .json config, .json results, threshold, metric name
-    my ($dbPath, $configPath, $resultPath, $threshold, $metric) = @_;
+sub applyThresholds {
+    # use given min/max thresholds to check if samples passed
+    # IMPORTANT: gender and identity pass/fail status is retained
+    my %min = %{shift()};
+    my %max = %{shift()};
+    my @metrics = @{shift()};
+    my %results = %{shift()};
+    my $logPath = shift;
     my %samplePass;
-    my %metricResults = readMetricResultHash($resultPath, $configPath);
-    foreach my $uri (keys(%metricResults)) {
-        my %results = %{$metricResults{$uri}};
-        my @result = @{$results{$metric}};
-        if ($result[1] >= $threshold) {
-            $samplePass{$uri} = 1;
-        } else {
-            $samplePass{$uri} = 0;
-        }
-    }
-    # now use %samplePass to update database
-    updateDatabase($dbPath, \%samplePass);
-}
-
-sub filterMetricsMultiple {
-    # filter on multiple metrics
-    my ($dbPath, $configPath, $resultPath, $metricThresholdsRef) = @_;
-    my %samplePass;
-    my %metricThresholds = %{$metricThresholdsRef};
-    my %metricResults = readMetricResultHash($resultPath, $configPath);
-    foreach my $uri (keys %metricResults) {
-        my %results = %{$metricResults{$uri}};
-        foreach my $metric (keys %metricThresholds) {
-            my ($threshold, $type) = @{$metricThresholds{$metric}};
-            my @result = @{$results{$metric}};
-            if ($type=='min' && $result[1] >= $threshold) {
-                $samplePass{$uri} = 1;
-            } elsif (($type=='max' && $result[1] <= $threshold)) {
-                $samplePass{$uri} = 1;
-            } else {
-                $samplePass{$uri} = 0;
+    my ($oldFail, $minFail, $maxFail) = (0,0,0);
+    my %fail;
+    my $failTotal = 0;
+    foreach my $metric (@metrics) { $fail{$metric}=0; }
+    foreach my $uri (keys %results) {
+        my $sampleOK = 1;
+        foreach my $metric (@metrics) {
+            my ($oldPass, $value) = @{$results{$uri}{$metric}};
+            my $newPass = 1;
+            if ($metric eq 'gender' || $metric eq 'identity' || 
+                $metric eq 'duplicate') {
+                $newPass = $oldPass;
+                unless ($oldPass) { $oldFail++; }
+            } elsif (defined($min{$metric}) && $value < $min{$metric}) {
+                $newPass = 0;
+                $minFail++;
+            } elsif (defined($max{$metric}) && $value > $max{$metric}) {
+                $newPass = 0;
+                $maxFail++;
             }
+            if ($newPass==0) { $sampleOK = 0; $fail{$metric}++; }
+        }
+        if (!$sampleOK) { $failTotal++; }
+        $samplePass{$uri} = $sampleOK;
+    }
+    if ($logPath) {
+        open my $log, ">", $logPath || croak "Cannot open log path $logPath";
+        foreach my $metric (@metrics) { 
+            print $log "Failed:$metric\t$fail{$metric}\n"; 
+        }
+        my $sampleTotal = keys(%results);
+        print $log "Total_failures\t$failTotal\n";
+        print $log "Total_samples\t$sampleTotal\n";
+        print $log "Pass_rate\t".(1 - $failTotal/$sampleTotal)."\n";
+        close $log || croak "Cannot close log path $logPath";
+    }
+    return %samplePass;
+}
+
+sub generateThresholds {
+    # parse thresholds for exclusion from a .json file
+    # thresholds may be simple min/max, or a standard deviation
+    # if standard deviation, need to use data to find min/max
+    my %config = %{shift()};
+    my %results = %{shift()};
+    my %thresholds = parseThresholds(%config);
+    my @metrics = @{$config{'name_array'}};
+    my %types = %{$config{'Threshold_types'}};
+    my (%min, %max);
+    foreach my $metric (@metrics) {
+        # find minimum/maximum for each metric
+        if ($types{$metric} eq 'Standard deviations') {
+            my @minmax = sigmaMinMax(\%results, $metric, $thresholds{$metric});
+            ($min{$metric}, $max{$metric}) = @minmax;
+        } elsif ($types{$metric} eq 'Minimum') {
+            $min{$metric} = $thresholds{$metric};
+        } elsif ($types{$metric} eq 'Maximum') {
+            $max{$metric} = $thresholds{$metric};
         }
     }
-    updateDatabase($dbPath, \%samplePass);
+    return (\%min, \%max, \@metrics);
+}
+
+sub runFilter {
+    # 'main' method to run prefilter for Illuminus/zCall
+    # Filter workflow:
+    # 1. read thresholds and types from JSON file 
+    # 2. read sample metrics from JSON file
+    # 3. compute standard deviations where required
+    # 4. apply min/max thresholds to identify samples as pass/fail
+    # 5. exclude failed samples from database
+    my %config = %{decode_json(readFileToString(shift()))};
+    my %results = %{decode_json(readFileToString(shift()))};
+    my $logPath = shift;
+    if (keys(%results)==0) { croak "Sample results input is empty"; }
+    my $dbPath = shift;
+    my ($minRef, $maxRef, $metricsRef) = generateThresholds(\%config,\%results);
+    my %samplePass = applyThresholds($minRef, $maxRef, $metricsRef, 
+                                     \%results, $logPath);
+    my ($pass, $total);
+    foreach my $uri (keys %samplePass) {
+        $total++;
+        if ($samplePass{$uri}) { $pass++; }
+    }
+    #updateDatabase($dbPath, \%samplePass);
+}
+
+sub sigmaMinMax {
+    # find min/max given data, metric name, and number of standard deviations
+    # assumes symmetric thresholds, ie. same number of sd's above/below mean
+    my ($resultsRef, $metric, $sdMax) = @_;
+    my %results = %{$resultsRef};
+    my @values;
+    foreach my $uri (keys %results) {
+        my ($pass, $value) = @{$results{$uri}{$metric}};
+        push @values, $value;
+    }
+    my ($mean, $sd) = meanSd(@values);
+    my $min = $mean - $sdMax * $sd;
+    my $max = $mean + $sdMax * $sd;
+    return ($min, $max);
 }
 
 sub updateDatabase {
