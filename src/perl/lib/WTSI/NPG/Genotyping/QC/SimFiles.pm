e7b2a511f36aec213e5bbdf73a415598d87a24f5
Iain Bancarz <ib5@sanger.ac.uk>
2013-08-16 11:50:45 +0100

    Handle infinite input intensities as well as NaNs

--- src/perl/lib/WTSI/NPG/Genotyping/QC/SimFiles.pm
+++ src/perl/lib/WTSI/NPG/Genotyping/QC/SimFiles.pm
@@ -66,12 +66,11 @@ struct simhead {
     int sampleUnitBytes;
 };
 
-void findMagByProbe(FILE *in, struct simhead header, float magByProbe[],
-		    char verbose);
+void findMagByProbe(FILE *in, struct simhead header, float magByProbe[]);
 void metricsFromFile(char* inPath, float mags[], float xyds[], char* names[],
-                     int *np, char verbose);
+                     char verbose);
 void readSampleProbes(FILE *in, int sampleOffset, struct simhead header, 
-		      float signals[], int *nans, char *name);
+                      float signals[], long *nans, char *name, char verbose);
 struct simhead readHeader(FILE *in);
 struct simhead readHeaderFromPath(char* inPath);
 void printHeader(struct simhead *h);
@@ -83,8 +82,7 @@ void PrintSimHeader(SV* args, ...);
 
 /*********************************************************/
 
-void findMagByProbe(FILE *in, struct simhead header, float magByProbe[],
-		    char verbose) {
+void findMagByProbe(FILE *in, struct simhead header, float magByProbe[]) {
   /* Find mean magnitude for each probe 
    Name and NaN count required for readSampleProbes, but not used here */
   int i, j;
@@ -93,13 +91,13 @@ void findMagByProbe(FILE *in, struct simhead header, float magByProbe[],
   float *signals;
   signals = calloc(totalSignals, sizeof(float));
   name = (char*) malloc(header.nameSize+1);
-  int nans = 0;
-  int *np = &nans;
+  long nans = 0;
+  char verbose = 1;
   for (i=0;i<header.probes;i++) { 
       magByProbe[i]=0.0; 
   }
   for (i=0;i<header.samples;i++) {
-      readSampleProbes(in, i, header, signals, np, name);
+      readSampleProbes(in, i, header, signals, &nans, name, verbose);
       for (j=0;j<totalSignals;j+=2) {
           float a = signals[j];
           float b = signals[j+1];
@@ -109,11 +107,10 @@ void findMagByProbe(FILE *in, struct simhead header, float magByProbe[],
   for (i=0;i<header.probes;i++) {
     magByProbe[i] = magByProbe[i] / header.samples;
   }
-  if (verbose) { printf("Found mean magnitude by probe.\n"); }
 }
 
 void metricsFromFile(char* inPath, float mags[], float xyds[], 
-                     char* names[], int *np, char verbose) {
+                     char* names[], char verbose) {
   /* Read a .sim file; find sample names and intensity metrics */
   FILE *in;
   in = fopen(inPath, "r");
@@ -137,17 +134,19 @@ void metricsFromFile(char* inPath, float mags[], float xyds[],
   char *name;
   name = (char*) malloc(header.nameSize+1);
   total = header.probes * header.channels;
-
-  if (verbose) { printf("First pass; about to find magnitude by probe.\n");  }
+  if (verbose) { printf("First pass; finding magnitude by probe.\n");  }
   // first pass -- find mean magnitude of intensity by probe
-  findMagByProbe(in, header, magByProbe, verbose);
+  findMagByProbe(in, header, magByProbe);
+  if (verbose) { printf("Second pass; finding metrics.\n"); }
   // second pass -- find xydiff and normalized magnitude for each sample
   int i;
+  long nans = 0;
+  char pVerbose = 0; // readSampleProbes was verbose in findMagByProbe
   for (i=0;i<header.samples;i++) {
-    readSampleProbes(in, i, header, signals, np, name);
-    strcpy(names[i], name); 
-    mags[i] = sampleMag(header.probes, signals, magByProbe);
-    xyds[i] = sampleXYDiff(header.probes, signals);
+      readSampleProbes(in, i, header, signals, &nans, name, pVerbose);
+      strcpy(names[i], name); 
+      mags[i] = sampleMag(header.probes, signals, magByProbe);
+      xyds[i] = sampleXYDiff(header.probes, signals);
   }
   if (verbose) { printf("Found intensity metrics.\n"); }
   /* Check that end of .sim file has been reached */
@@ -161,11 +160,15 @@ void metricsFromFile(char* inPath, float mags[], float xyds[],
   if (status != 0) {
       fprintf(stderr, "ERROR: Could not close .sim file %s\n");
       exit(1);
-  } else if (verbose) { printf("NaNs found:%d\n", *np); }
+  } else if (nans>0) { 
+      fprintf(stderr, "NaN and infinities found: %ld\nAll NaN and infinite intensities set to zero when computing metrics.\n", nans); 
+  } else if (verbose) { 
+      printf("Finished; no NaN/infinity inputs found.\n");  
+  }
 }
 
 void readSampleProbes(FILE *in, int sampleOffset, struct simhead header, 
-		      float signals[], int *nans, char *name) {
+                      float signals[], long *nans, char *name, char verbose) {
     /* Read intensities for the Nth sample in the file
        Record number of NaN intensites, and convert NaN values to 0 
 
@@ -183,8 +186,6 @@ void readSampleProbes(FILE *in, int sampleOffset, struct simhead header,
   result = fseeko(in, start, 0); 
   if (result!=0) {  
     fprintf(stderr, "ERROR: Seek failed in .sim file.\n");  
-    fprintf(stderr, "OFFSET:%llu\n", offsetL);
-    fprintf(stderr, "SAMPLE_UNIT_BYTES:%llu\n", sampleUnitBytesL);
     fprintf(stderr, "ATTEMPTED_SEEK_POSITION:%llu\n", start);
     fprintf(stderr, "TELL_POSITION:%llu\n", ftello(in));
     exit(1);
@@ -195,14 +196,27 @@ void readSampleProbes(FILE *in, int sampleOffset, struct simhead header,
       fprintf(stderr, "ERROR: Failed to read sample data from input .sim\n");
       exit(1);
   }
-  // loop over signals, convert nan's to 0 and count nan's
+  /* loop over signals, convert NaN/infinity to 0 and count occurrences
+     Update a global count of nans/infinities, and local counts for sample
+     Warn if sample nan/infinity count is greater than zero
+   */
+  //printf("###%ld\n", *nans);
   int i;
-  *nans = 0;
+  int nanCount = 0;
+  int infCount = 0;
   for (i=0;i<signalTotal;i++) {
       if (isnan(signals[i])) {
           signals[i] = 0;
-          *nans++;
+          (*nans)++;
+          nanCount++;
+      } else if (signals[i]==INFINITY || signals[i]==-INFINITY) {
+          signals[i]=0;
+          (*nans)++;
+          infCount++;
       }
+  }
+  if (verbose && (nanCount > 0 || infCount > 0)) {
+      fprintf(stderr, "Warning: Sample offset %d, name %s: Found %d NaN values, %d infinities.\n", sampleOffset, name, nanCount, infCount);
   }
 }
 
@@ -355,13 +369,7 @@ void FindMetrics(SV* args, ...) {
         mags[i] = 0.0;
         xyds[i] = 0.0;
     }
-    int nans = 0; // NaN counter
-    int *np;
-    np = &nans;
-    metricsFromFile(inPath, mags, xyds, names, np, verbose);
-    if (*np > 0) {
-        fprintf(stderr, "Warning: %d NaN values found in .sim file.\n", *np);
-    }
+    metricsFromFile(inPath, mags, xyds, names, verbose);
     writeResults(magPath, header.samples, names, mags);
     writeResults(xydPath, header.samples, names, xyds);
     if (verbose) { printf("Finished.\n"); }
