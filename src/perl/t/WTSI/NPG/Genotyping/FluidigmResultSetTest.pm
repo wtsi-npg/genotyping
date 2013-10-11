9bc3f1fc5a4e8eaece3972370b92b462d505cae1
Keith James <kdj@sanger.ac.uk>
2013-10-11 12:24:33 +0100

    Added tests for specific parts of the run data being missing

--- src/perl/t/WTSI/NPG/Genotyping/FluidigmResultSetTest.pm
+++ src/perl/t/WTSI/NPG/Genotyping/FluidigmResultSetTest.pm
@@ -7,7 +7,7 @@ use strict;
 use warnings;
 
 use base qw(Test::Class);
-use Test::More tests => 12;
+use Test::More tests => 15;
 use Test::Exception;
 
 Log::Log4perl::init('etc/log4perl_tests.conf');
@@ -16,20 +16,36 @@ BEGIN { use_ok('WTSI::NPG::Genotyping::FluidigmResultSet'); }
 
 use WTSI::NPG::Genotyping::FluidigmResultSet;
 
-my $result_dir = './t/fluidigm_result_set';
+my $result_dir = './t/fluidigm_result_set/complete/0123456789';
+
+my $missing_data = './t/fluidigm_result_set/missing_data/0123456789';
+my $missing_export = './t/fluidigm_result_set/missing_export/0123456789';
+my $missing_tif = './t/fluidigm_result_set/missing_tif/0123456789';
 my $missing_dir = './t/fluidigm_noplace';
 
 sub require : Test(1) {
   require_ok('WTSI::NPG::Genotyping::FluidigmResultSet');
 };
 
-sub constructor : Test(2) {
+sub constructor : Test(5) {
   new_ok('WTSI::NPG::Genotyping::FluidigmResultSet',
          [directory => $result_dir]);
 
   dies_ok { WTSI::NPG::Genotyping::FluidigmResultSet->new
       (directory => $missing_dir) }
-    "Expected to fail when directory argument does not exist";
+    "Expected to fail when the directory does not exist";
+
+  dies_ok { WTSI::NPG::Genotyping::FluidigmResultSet->new
+      (directory => $missing_data) }
+    "Expected to fail when the Data directory does not exist";
+
+  dies_ok { WTSI::NPG::Genotyping::FluidigmResultSet->new
+      (directory => $missing_export) }
+    "Expected to fail when the export file does not exist";
+
+  dies_ok { WTSI::NPG::Genotyping::FluidigmResultSet->new
+      (directory => $missing_tif) }
+    "Expected to fail when a tif file does not exist";
 };
 
 sub paths : Test(8) {
@@ -43,12 +59,12 @@ sub paths : Test(8) {
      "data_directory is 'Data/$result_dir'");
   ok(-d $result_set->data_directory, 'Data directory exists');
 
-  is($result_set->export_file, "$result_dir/fluidigm_result_set.csv",
+  is($result_set->export_file, "$result_dir/0123456789.csv",
      "export_file is '$result_dir'");
   ok(-f $result_set->export_file, 'Export file exists');
 
-  is($result_set->fluidigm_barcode, "fluidigm_result_set",
-     "fluidigm_barcode is 'fluidigm_result_set'");
+  is($result_set->fluidigm_barcode, '0123456789',
+     "fluidigm_barcode is '0123456789'");
 
   is_deeply($result_set->tif_files, ["$result_dir/Data/aramis.tif",
                                      "$result_dir/Data/athos.tif",
