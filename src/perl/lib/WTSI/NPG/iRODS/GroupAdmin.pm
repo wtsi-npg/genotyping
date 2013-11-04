596a5377af6dd84acffeda9a11c3f9800db92e81
David K Jackson <david.jackson@sanger.ac.uk>
2013-11-04 16:40:09 +0000

    rationalise code removing debug structures which were in place get initial working version

--- src/perl/lib/WTSI/NPG/iRODS/GroupAdmin.pm
+++ src/perl/lib/WTSI/NPG/iRODS/GroupAdmin.pm
@@ -2,59 +2,45 @@ package WTSI::NPG::iRODS::GroupAdmin;
 use Moose;
 use IPC::Run qw(start);
 
-sub str_ref {
-                 my $tmpstr='';
-                 return \$tmpstr;
-}
-
-has '_foo' => (
-  'is' => 'ro',
-  'isa' => 'HashRef',
-  'default' => sub {return {in=>'', out=>''} },
-);
-
 has '_in' => (
   'is' => 'ro',
-  'isa' => 'ScalarRef[Str]',
-  'default' => sub {str_ref},
+  'isa' => 'ScalarRef',
+  'default' => sub {my$t=''; \$t},
 );
 
 has '_out' => (
   'is' => 'ro',
-  'isa' => 'ScalarRef[Str]',
-  'default' => sub {str_ref},
+  'isa' => 'ScalarRef',
+  'default' => sub {my$t=''; \$t},
 );
 
 
 has '_harness' => (
   'is' => 'ro',
-  'default' => sub {
+  'builder' => '_build__harness',
+  'lazy' => 1
+);
+
+sub _build__harness {
                  my ($self) = @_;
-                 #my $out_ref = $self->_out;
-                 my$foo=$self->_foo;
-use Data::Dumper;
-warn Dumper [$self->_foo,$foo,$foo->{out},\$foo->{out}];
-                 my $out_ref = \$foo->{out};
-warn $out_ref;
-                 #my $in_ref = $self->_in;
-                 my $in_ref = \$foo->{in};
-warn $in_ref;
+                 my $out_ref = $self->_out;
+#use Data::Dumper;
+#warn Dumper [$self];
+#warn $out_ref;
+                 my $in_ref = $self->_in;
+#warn $in_ref;
                  my $h = start [qw(igroupadmin)], q(<pty<), $in_ref, q(>pty>), $out_ref;
                  while (1){ $h->pump; last if ${$out_ref}=~/^groupadmin\>/sm; }
                  ${$out_ref}=q();
                  return $h;
-               }
-);
+}
 
 sub lg {
   my $self = shift;
-  #${$self->_out}=q();
-  my$foo=$self->_foo;
-  my $out_ref = \$foo->{out};
+  my $out_ref = $self->_out;
   ${$out_ref}=q();
-  #${$self->_in}.= join q( ), q(lg), @_, qq(\n);
   my $in = join q( ), q(lg), @_, qq(\n);
-  $foo->{in} = $in;
+  ${$self->_in} = $in;
   while (1){ $self->_harness->pump ; last if ${$out_ref}=~s/\r?\n\r?^groupadmin\>//sm; }
   ${$out_ref}=~s/\r//smg;
   ${$out_ref}=~s/\A$in//sm;
@@ -68,10 +54,8 @@ sub lg {
 
 sub DEMOLISH {
   my ($self) = @_;
-  #${$self->_out}=q();
-  ${\$self->_foo->{out}}=q();
-  #${$self->_in}="quit\n";
-  ${\$self->_foo->{in}}="quit\n";
+  ${$self->_out}=q();
+  ${$self->_in}="quit\n";
   $self->_harness->finish;
 }
 
