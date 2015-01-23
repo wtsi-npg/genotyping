2a9152f9b0b60d27e4221aaab5cac680195b58da
iainrb <ib5@sanger.ac.uk>
2015-01-23 16:30:00 +0000

    Script to cross-check Infinium LIMS, iRODS, and SequenceScape warehouse for a given project

---
+++ src/perl/bin/query_project_samples.pl
@@ -0,0 +1,197 @@
+#!/software/bin/perl
+
+use utf8;
+
+package main;
+
+use warnings;
+use strict;
+use Getopt::Long;
+use Log::Log4perl;
+use Log::Log4perl::Level;
+use Pod::Usage;
+
+use WTSI::NPG::Database::Warehouse;
+use WTSI::NPG::Genotyping::Database::Infinium;
+use WTSI::NPG::Genotyping::Infinium::SampleQuery;
+use WTSI::NPG::iRODS;
+use WTSI::NPG::iRODS::MetaSearcher;
+use WTSI::NPG::Utilities qw(user_session_log);
+
+our $DEFAULT_INI = $ENV{HOME} . "/.npg/genotyping.ini";
+our $EXPECTED_IRODS_FILES = 3;
+our @DATA_SOURCE_NAMES = qw(LIMS_ IRODS SS_WH);
+our @HEADER_FIELDS = qw(data_source plate well sample infinium_beadchip
+                        infinium_beadchip_section sequencescape_barcode);
+
+my $uid = `whoami`;
+chomp($uid);
+my $session_log = user_session_log($uid, 'query_project_samples');
+
+my $embedded_conf = "
+   log4perl.logger.npg.irods.publish = WARN, A1, A2
+
+   log4perl.appender.A1           = Log::Log4perl::Appender::Screen
+   log4perl.appender.A1.utf8      = 1
+   log4perl.appender.A1.layout    = Log::Log4perl::Layout::PatternLayout
+   log4perl.appender.A1.layout.ConversionPattern = %d %p %m %n
+
+   log4perl.appender.A2           = Log::Log4perl::Appender::File
+   log4perl.appender.A2.filename  = $session_log
+   log4perl.appender.A2.utf8      = 1
+   log4perl.appender.A2.layout    = Log::Log4perl::Layout::PatternLayout
+   log4perl.appender.A2.layout.ConversionPattern = %d %p %m %n
+   log4perl.appender.A2.syswrite  = 1
+";
+
+my $log;
+
+run() unless caller();
+
+sub run {
+
+    my $config;
+    my $debug_level;
+    my $header;
+    my $limit;
+    my $outpath;
+    my $project;
+    my $quiet;
+    my $root;
+    my $log4perl_config;
+    my $verbose;
+
+    GetOptions(
+        'config=s'         => \$config,
+        'debug'            => \$debug_level,
+        'header'           => \$header,
+        'help'             => sub { pod2usage(-verbose => 2,
+                                              -exitval => 0) },
+        'limit=i'          => \$limit,
+        'logconf=s'        => \$log4perl_config,
+        'out=s'            => \$outpath,
+        'project=s'        => \$project,
+        'quiet'            => \$quiet,
+        'root=s'           => \$root,
+        'verbose'          => \$verbose);
+
+    $config         ||= $DEFAULT_INI;
+    unless ($project) {
+        pod2usage(-msg => "A --project argument is required\n", -exitval => 2);
+    }
+    if ($limit <= 0) {
+        pod2usage(-msg => "--limit argument must be > 0\n", -exitval => 2);
+    }
+    if ($log4perl_config) {
+        Log::Log4perl::init($log4perl_config);
+        $log = Log::Log4perl->get_logger('npg.irods.publish');
+    }
+    else {
+        Log::Log4perl::init(\$embedded_conf);
+        $log = Log::Log4perl->get_logger('npg.irods.publish');
+        if ($debug_level) {
+            $log->level($DEBUG);
+        } elsif ($verbose) {
+            $log->level($INFO);
+        } elsif ($quiet) {
+            $log->level($ERROR);
+        }
+    }
+
+    my $ifdb = WTSI::NPG::Genotyping::Database::Infinium->new
+        (name    => 'infinium',
+         inifile => $config,
+         logger  => $log)->connect(RaiseError => 1);
+
+    my $ssdb = WTSI::NPG::Database::Warehouse->new
+        (name    => 'sequencescape_warehouse',
+         inifile => $config,
+         logger  => $log)->connect(RaiseError           => 1,
+                                   mysql_enable_utf8    => 1,
+                                   mysql_auto_reconnect => 1);
+
+    $root           ||= '/archive';
+    if ($root !~ '^/') { $root = '/'.$root; }
+
+    my $sample_query = WTSI::NPG::Genotyping::Infinium::SampleQuery->new
+        (ifdb   => $ifdb,
+         ssdb   => $ssdb,
+         logger => $log);
+
+    $sample_query->run($project, $root, $outpath, $header, $limit);
+}
+
+
+__END__
+
+=head1 NAME
+
+query_project_samples
+
+=head1 SYNOPSIS
+
+query_project_samples [--config <database .ini file>] \
+   [--header] [--limit <n>] [--help] [--out <path>] \
+   --project <project name> [--quiet] [ --root <irods root> ] [--verbose]
+
+Options:
+
+  --config        Load database configuration from a user-defined .ini file.
+                  Optional, defaults to $HOME/.npg/genotyping.ini
+  --header        Print a header line at the start of output (if any).
+                  Optional.
+  --help          Display help.
+  --limit         Maximum number of samples to query in iRODS and
+                  SequenceScape. Optional.
+  --out           Path for output, or - for STDOUT. Optional; if omitted, no
+                  output will be written.
+  --project       Name of an Infinium LIMS project to query. Required.
+  --quiet         Do not write warning messages; only log more serious
+                  errors. Optional.
+  --root          Root path of iRODS zone to query. Optional, defaults
+                  to /archive.
+  --verbose       Print additional messages while processing. Optional.
+
+=head1 DESCRIPTION
+
+Program to retrieve information about all samples in a given project from
+the Infinium LIMS, iRODS, and the SequenceScape Warehouse.
+
+The given project name is used to query the LIMS and obtain a list of
+samples. The details of each sample are then used to query iRODS and
+SequenceScape.
+
+Warnings will be raised if:
+- The iRODS query does not return exactly three data objects (we expect
+  two IDATs and one GTC)
+- SequenceScape does not return results for a given sample
+
+If the --out option is given, the program will output three lines for each
+sample: One each for the LIMS, iRODS, and SequenceScape. Each line contains
+comma-separated values, which may be empty if the information is not
+available, or not relevant to a given data source. To also write a brief
+description of each field, run with the --header option.
+
+=head1 METHODS
+
+None
+
+=head1 AUTHOR
+
+Iain Bancarz <ib5@sanger.ac.uk>
+
+=head1 COPYRIGHT AND DISCLAIMER
+
+Copyright (c) 2015 Genome Research Limited. All Rights Reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the Perl Artistic License or the GNU General
+Public License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+=cut
---
+++ src/perl/lib/WTSI/NPG/Genotyping/Infinium/SampleQuery.pm
@@ -0,0 +1,278 @@
+use utf8;
+
+package WTSI::NPG::Genotyping::Infinium::SampleQuery;
+
+use Moose;
+
+use warnings;
+use strict;
+use Log::Log4perl;
+use Log::Log4perl::Level;
+use Pod::Usage;
+
+use WTSI::NPG::Database::Warehouse;
+use WTSI::NPG::Genotyping::Database::Infinium;
+use WTSI::NPG::iRODS;
+use WTSI::NPG::iRODS::MetaSearcher;
+use WTSI::NPG::Utilities qw(user_session_log);
+
+with 'WTSI::DNAP::Utilities::Loggable';
+
+our $EXPECTED_IRODS_FILES = 3;
+our @DATA_SOURCE_NAMES = qw(LIMS_ IRODS SS_WH);
+our @HEADER_FIELDS = qw(data_source plate well sample infinium_beadchip
+                        infinium_beadchip_section sequencescape_barcode);
+
+has 'ifdb'  =>
+  (is       => 'ro',
+   isa      => 'WTSI::NPG::Genotyping::Database::Infinium',
+   required => 1);
+
+has 'irods' =>
+  (is       => 'ro',
+   isa      => 'WTSI::NPG::iRODS',
+   required => 1,
+   default  => sub {
+     return WTSI::NPG::iRODS->new;
+   });
+
+has 'ssdb'  =>
+  (is       => 'ro',
+   isa      => 'WTSI::NPG::Database::Warehouse',
+   required => 1);
+
+sub BUILD {
+  my ($self) = @_;
+
+  # Make our irods handle use our logger by default
+  $self->irods->logger($self->logger);
+}
+
+=head2 run
+
+  Arg [1]    : string project name
+  Arg [2]    : string iRODS root path
+  Arg [3]    : string output path (or - for STDOUT) (optional)
+  Arg [4]    : boolean, write header to output (if any) (optional)
+  Arg [5]    : integer, maximum samples to query (optional)
+  Example    : $sq->run('my_project', '/archive', '-', 1, 5)
+  Description: Run a query against the Infinium LIMS, iRODS, and
+               SequenceScape database for a given project. Retrieve
+               information for each sample in the project. Optionally,
+               can write results to file or STDOUT, and/or log any
+               discrepancies.
+  Returntype : null
+  Caller     : query_project_samples.pl
+
+=cut
+
+sub run {
+    my ($self, $project, $root, $outpath, $header, $limit) = @_;
+    unless ($project && $root) {
+        # sanity check; main validation of arguments occurs in calling script
+        $self->logcroak("Project and iRODS root arguments are required");
+    }
+
+    # query data sources: Infinium LIMS, iRODS, SequenceScape warehouse
+    my @infinium_data = $self->_find_infinium_data($project);
+    my $if_total = scalar @infinium_data;
+    $self->info("Found ", $if_total, " Infinium samples.");
+    if ($limit) {
+        if ($limit >= $if_total) {
+            $self->info("Sample limit of ", $limit, " is not less than ",
+                        "number found; continuing with all samples.");
+        } else {
+            @infinium_data = splice(@infinium_data, 0, $limit);
+            $self->info("Reduced list of samples to match limit of ", $limit);
+            $if_total = $limit;
+        }
+    }
+    my @irods_data = $self->_find_irods_metadata(\@infinium_data, $root);
+    my $irods_total = scalar @irods_data;
+    $self->info("Found ", $irods_total, " iRODS samples.");
+    my @warehouse_data = $self->_find_warehouse_data(\@infinium_data);
+    my $wh_total = scalar @warehouse_data;
+    $self->info("Found ", $wh_total, " warehouse samples.");
+
+    # compare sample totals and output
+    if ($if_total == $irods_total && $irods_total == $wh_total) {
+        $self->info("Sample counts in Infinium, iRODS and ",
+                    "Sequencescape are identical: ", $if_total,
+                    " samples found.");
+        if ($outpath) {
+            if ($outpath eq '-') { $self->info("Writing output to STDOUT"); }
+            else { $self->info("Writing output to $outpath"); }
+            my @all_results = ( [@infinium_data], [@irods_data],
+                                [@warehouse_data] );
+            $self->_write_output(\@all_results, $outpath, $header);
+        }
+    } else {
+        $self->logcroak("Sample counts do not match: Found ", $if_total,
+                        " in Infinium, ", $irods_total, " in iRODS, ",
+                        $wh_total, " in Sequencescape.");
+    }
+
+    return;
+}
+
+sub _find_infinium_data {
+    # query Infinium LIMS DB to get samples
+    # extract relevant fields and store in array of arrays
+    my ($self, $project) = @_;
+    my @if_samples = @{$self->ifdb->find_project_samples($project)};
+    my @data;
+    foreach my $if_sample (@if_samples) {
+        push @data, [$if_sample->{'plate'},
+                     $if_sample->{'well'},
+                     $if_sample->{'sample'},
+                     $if_sample->{'beadchip'},
+                     $if_sample->{'beadchip_section'},
+                     '' # placeholder for WH barcode
+                  ];
+    }
+    return @data;
+}
+
+sub _find_irods_metadata {
+    # given a list of metadata values, cross-check with irods
+    # root is the path to an irods zone, eg. /archive
+    # for each input, require:
+    # - 3 files in iRODS: red IDAT, green IDAT, GTC
+    # - Consistent values for each field between 3 files
+    # Required fields (all correspond to Infinium LIMS values):
+    # - infinium_plate
+    # - infinium_well
+    # - infinium_sample
+    # - beadchip
+    # - beadchip_section
+    # if metadata is incorrect or missing for a given sample,
+    #  return empty values
+    my ($self, $inputs_ref, $root) = @_;
+    my @metadata;
+    foreach my $input (@{$inputs_ref}) {
+        my ($plate, $well, $sample, $beadchip, $beadchip_section) = @{$input};
+        my @irods_query = (['infinium_plate', $plate ],
+                           ['infinium_well', $well],
+                           ['infinium_sample', $sample],
+                           ['beadchip', $beadchip],
+                           ['beadchip_section', $beadchip_section]
+                       );
+        my @results = $self->irods->find_objects_by_meta($root, @irods_query);
+        my $total = scalar @results;
+        $self->debug("Found ", $total, " iRODS results for plate ",
+                     $plate, ", well ", $well, ", sample ", $sample);
+        if ($total != $EXPECTED_IRODS_FILES) {
+            $self->logwarn("Expected ", $EXPECTED_IRODS_FILES,
+                           " files in iRODS, found ", $total,
+                           " for plate '", $plate,
+                           "', well '", $well, "', sample '", $sample,
+                           "', beadchip '", $beadchip, "', section '",
+                           $beadchip_section, "'");
+            push @metadata, ['', '', '', '', '', '' ];
+        } else {
+            push @metadata, [$plate, $well, $sample,
+                             $beadchip, $beadchip_section,
+                             '']; # placeholder for WH barcode
+        }
+    }
+    return @metadata;
+}
+
+sub _find_warehouse_data {
+    my ($self, $inputs_ref) = @_;
+    my @data;
+    foreach my $input (@{$inputs_ref}) {
+        my ($plate, $well, $sample) = @{$input};
+        my $wh_result = $self->ssdb->find_infinium_sample_by_plate($plate,
+                                                                   $well);
+        my @result;
+        if ($wh_result) {
+            @result = ($plate, $well, $wh_result->{'name'},
+                       '', '', # placeholders for Infinium beadchip & section
+                       $wh_result->{'barcode'});
+        } else {
+            $self->logwarn("No SequenceScape Warehouse result for plate '",
+                           $plate, "', well '", $well,
+                           "', Infinium sample name '", $sample, "'");
+            @result = ($plate, $well, '', '', '', '');
+        }
+        push (@data, [@result]);
+    }
+    return @data;
+}
+
+sub _write_output {
+    my ($self, $results_ref, $outpath, $header) = @_;
+    # check the number of samples and data sources
+    my @all_results = @{$results_ref};
+    if (scalar(@all_results) != scalar(@DATA_SOURCE_NAMES)) {
+        $self->logcroak("Incorrect number of result arrays input: ",
+                        "Expected ", scalar(@DATA_SOURCE_NAMES), " got ",
+                        scalar(@all_results));
+    }
+    my $total_samples = -1;
+    foreach my $data (@all_results) {
+        if ($total_samples == -1) {
+            $total_samples = scalar(@{$data})
+        } elsif (scalar(@{$data}) != $total_samples) {
+            $self->logcroak("Inconsistent numbers of samples ",
+                            "between data sources");
+        }
+    }
+    # now write the output
+    my $out;
+    if ($outpath eq '-') {
+        $out = *STDOUT;
+    } else {
+        open $out, ">", $outpath ||
+            $self->logcroak("Cannot open output path '$outpath': $!");
+    }
+    if ($header) {
+        print $out join(',', @HEADER_FIELDS)."\n";
+    }
+    for (my $i=0;$i<$total_samples;$i++) {
+        for (my $j=0;$j<@all_results;$j++) {
+            my @result = @{$all_results[$j][$i]};
+            unshift(@result, $DATA_SOURCE_NAMES[$j]);
+            if (scalar(@result) != scalar(@HEADER_FIELDS)) {
+                $self->logcroak("Incorrect number of fields in result: ",
+                                "Expected ", scalar(@HEADER_FIELDS),
+                                " got ", scalar(@result));
+            }
+            print $out join(',', @result)."\n";
+        }
+    }
+    if ($outpath ne '-') {
+        close $out ||
+            $self->logcroak("Cannot close output path '$outpath': $!");
+    }
+    return;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+no Moose;
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Iain Bancarz <ib5@sanger.ac.uk>
+
+=head1 COPYRIGHT AND DISCLAIMER
+
+Copyright (c) 2015 Genome Research Limited. All Rights Reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the Perl Artistic License or the GNU General
+Public License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+=cut
