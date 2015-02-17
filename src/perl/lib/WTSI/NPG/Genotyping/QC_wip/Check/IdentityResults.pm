203872fbcf963ea13e6dfb352070d6c1a04811d0
iainrb <ib5@sanger.ac.uk>
2015-02-17 14:29:16 +0000

    Added extra sanity checks, renamed variables

--- src/perl/lib/WTSI/NPG/Genotyping/QC_wip/Check/IdentityResults.pm
+++ src/perl/lib/WTSI/NPG/Genotyping/QC_wip/Check/IdentityResults.pm
@@ -24,8 +24,9 @@ with 'WTSI::DNAP::Utilities::Loggable';
 #                 failed   => <Bool>,
 #                 sample   => <Str>}
 
-# need to get failed results, as input to evaluate_sample_swaps
-# convert to a JSON-compatible data structure for output
+# need to:
+# - get failed results, as input to evaluate_sample_swaps
+# - convert to a JSON-compatible data structure for output
 
 # populate an IdentityResults object 'on the fly' while doing identity check
 
@@ -59,17 +60,23 @@ sub BUILD {
                         scalar(@{$self->sample_names}), " names, ",
                         scalar(@{$self->results}), " results" );
     }
-
+    foreach my $sample (@{$self->sample_names}) {
+        if (defined($self->sample_names_hash->{$sample})) {
+            $self->logcroak("Sample '", $sample,
+                            "' occurs more than once in inputs");
+        }
+        $self->sample_names_hash->{$sample} = 1;
+    }
 }
 
 sub add_sample_result {
     my ($self, $sample, $result) = @_;
     # enforce uniqueness of sample names
-    if ($self->sample_names_hash->{$sample}) {
+    if (defined($self->sample_names_hash->{$sample})) {
         $self->logcroak("Sample name '$sample' already exists in result set");
     }
-    $self->sample_names_hash->{$sample} = 1;
     push (@{$self->sample_names}, $sample);
+    $self->sample_names_hash->{$sample} = 1;
     # sanity checking on result argument
     my @keys = qw/match mismatch identity missing failed/;
     foreach my $key (@keys) {
@@ -83,27 +90,45 @@ sub add_sample_result {
 
 sub delete_sample_result {
     my ($self, $target) = @_;
-    my $i = 0;
-    my @sample_names = @{$self->sample_names};
-    foreach my $sample (@sample_names) {
-        if ($sample eq $target) {
-            splice(@sample_names, $i, 1);
-            last;
-        } else {
-            $i++;
+    if ($self->sample_names_hash->{$target}) {
+        my $i = 0;
+        my @sample_names = @{$self->sample_names};
+        foreach my $sample (@sample_names) {
+            if ($sample eq $target) {
+                splice(@sample_names, $i, 1);
+                last;
+            } else {
+                $i++;
+            }
         }
+        delete $self->sample_names_hash->{$target};
+        $self->sample_names(\@sample_names);
+        splice(@{$self->results}, $i, 1);
+    } else {
+        $self->logwarn("Attempted to delete sample '", $target,
+                       "', but no sample of that name exists");
     }
-    $self->sample_names(\@sample_names);
-    splice(@{$self->results}, $i, 1);
     return 1;
 }
+
+
+=head2 get_failed_results
+
+  Arg [1]    : None
+
+  Example    : my $failed = @{$results->get_failed_results};
+  Description: Return a new IdentityResults object, containing results from
+               the current object whose QC status is 'failed'.
+  Returntype : WTSI::NPG::Genotyping::QC_wip::Check::IdentityResults
+
+=cut
 
 sub get_failed_results {
     my ($self) = @_;
     my $failed_results =
         WTSI::NPG::Genotyping::QC_wip::Check::IdentityResults->new();
     my $i = 0;
-    while ($i < $self->get_num_results) {
+    while ($i < $self->get_num_samples) {
         if ($self->results->[$i]->{'failed'}) {
             $failed_results->add_sample_result($self->sample_names->[$i],
                                                $self->results->[$i]);
@@ -113,16 +138,57 @@ sub get_failed_results {
     return $failed_results;
 }
 
-sub get_num_results {
+
+=head2 get_num_samples
+
+  Arg [1]    : None
+
+  Example    : my $n = $results->get_num_samples;
+  Description: Return number of samples in the Plink data.
+  Returntype : Int
+
+=cut
+
+sub get_num_samples {
     my ($self) = @_;
     return scalar(@{$self->sample_names});
 }
+
+
+=head2 get_sample_names
+
+  Arg [1]    : None
+
+  Example    : my @names = @{$results->get_sample_names};
+  Description: Return the names of the samples in the Plink data, in
+               their original order.
+  Returntype : ArrayRef[Str]
+
+=cut
+
+sub get_sample_names {
+   my ($self) = @_;
+   return $self->sample_names;
+}
+
+=head2 to_json_spec
+
+  Arg [1]    : None
+
+  Example    : my @json_spec = @{$results->to_json_spec};
+  Description: Transform the IdentityResults object into a data structure
+               which can be converted to JSON, by converting
+               WTSI::NPG::Genotyping::Call objects to strings.
+  Returntype : ArrayRef
+
+=cut
+
 
 sub to_json_spec {
    my ($self) = @_;
    my @identity_results_json;
    my $i = 0;
-   while ($i < $self->get_num_results) {
+   while ($i < $self->get_num_samples) {
        my $result_spec = $self->_make_result_json_spec($self->results->[$i]);
        push(@identity_results_json, [ $self->sample_names->[$i],
                                       $result_spec] );
