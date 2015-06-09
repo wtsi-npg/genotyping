6cfcf13d7ad9d73627d2ecdf0e7f4927858f1cdf
Keith James <kdj@sanger.ac.uk>
2015-06-09 14:36:39 +0100

    Critic: InputOutput::RequireEncodingWithUTF8Layer

--- src/perl/lib/WTSI/NPG/Expression/ChipLoadingManifestV2.pm
+++ src/perl/lib/WTSI/NPG/Expression/ChipLoadingManifestV2.pm
@@ -19,7 +19,7 @@ sub BUILD {
     $self->logdie("Manifest file '", $self->file_name,
                   "' does not exist");
 
-  open my $in, '<:encoding(utf8)', $self->file_name
+  open my $in, '<:encoding(UTF-8)', $self->file_name
     or $self->logdie("Failed to open Manifest file '",
                      $self->file_name, "': $!");
 
@@ -51,7 +51,6 @@ sub BUILD {
 # Any whitespace-only lines are ignored.
 sub _parse_beadchip_table {
   my ($self, $fh) = @_;
-  binmode($fh, ':utf8');
 
   # Channel is always Grn (Cy3)
   my $channel = 'Grn';
