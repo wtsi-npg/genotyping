df09dd326e41befc217f2a2e055f1bd5dca15cf3
iainrb <ib5@sanger.ac.uk>
2015-06-11 11:39:32 +0100

    Updated copyright dates

--- src/perl/lib/WTSI/NPG/Genotyping/Types.pm
+++ src/perl/lib/WTSI/NPG/Genotyping/Types.pm
@@ -128,7 +128,7 @@ Keith James <kdj@sanger.ac.uk>
 
 =head1 COPYRIGHT AND DISCLAIMER
 
-Copyright (c) 2014 Genome Research Limited. All Rights Reserved.
+Copyright (c) 2014, 2015 Genome Research Limited. All Rights Reserved.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the Perl Artistic License or the GNU General
