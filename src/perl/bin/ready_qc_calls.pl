3cd0204de4a07cfacc6d04b37b61b6d1865a8a88
iainrb <ib5@sanger.ac.uk>
2016-02-18 15:51:14 +0000

    Change copyright years

--- src/perl/bin/ready_qc_calls.pl
+++ src/perl/bin/ready_qc_calls.pl
@@ -201,7 +201,7 @@ Iain Bancarz <ib5@sanger.ac.uk>
 
 =head1 COPYRIGHT AND DISCLAIMER
 
-Copyright (C) 2015 Genome Research Limited. All Rights Reserved.
+Copyright (C) 2015, 2016 Genome Research Limited. All Rights Reserved.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the Perl Artistic License or the GNU General
