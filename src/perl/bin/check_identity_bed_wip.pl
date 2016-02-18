87b8b47db3bde22b0e8f3b4f8511f42fa70635cf
iainrb <ib5@sanger.ac.uk>
2016-02-18 14:16:15 +0000

    Updated copyright years

--- src/perl/bin/check_identity_bed_wip.pl
+++ src/perl/bin/check_identity_bed_wip.pl
@@ -304,7 +304,7 @@ Iain Bancarz <ib5@sanger.ac.uk>
 
 =head1 COPYRIGHT AND DISCLAIMER
 
-Copyright (c) 2015 Genome Research Limited. All Rights Reserved.
+Copyright (c) 2015, 2016 Genome Research Limited. All Rights Reserved.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the Perl Artistic License or the GNU General
--- src/ruby/genotyping-workflows/lib/genotyping/tasks/quality_control.rb
+++ src/ruby/genotyping-workflows/lib/genotyping/tasks/quality_control.rb
@@ -1,6 +1,6 @@
 #-- encoding: UTF-8
 #
-# Copyright (c) 2012 Genome Research Ltd. All rights reserved.
+# Copyright (c) 2012, 2016 Genome Research Ltd. All rights reserved.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
